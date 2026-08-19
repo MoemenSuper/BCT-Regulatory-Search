3f1858e6ffa27ae52dd3c7a3c4fac266d50acdb3
MoemenSuper <ouerghuimoemej@gmail.com>
2026-08-19 19:49:58 +0100

    Introducting bm25 for hybrid Retrieval.

---
+++ bm25.py
@@ -0,0 +1,17 @@
+from langchain_core.documents import Document
+
+def load_documents_from_chroma(vector_store):
+
+    results = vector_store.get(include=["documents", "metadatas"])
+    documents = []
+
+    for text, metadata in zip(results["documents"], results["metadatas"]):
+
+        document = Document(
+            page_content = text,
+            metadata = metadata
+        )
+
+        documents.append(document)
+
+    return documents
